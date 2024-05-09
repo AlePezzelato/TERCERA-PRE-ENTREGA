2d8266bcd423c18929f7ca3d7617b9ce9de529f1
AlePezzelato <apezzelato1@gmail.com>
2024-05-09 18:38:03 -0300

    creacion de apps alumno, docente y materia

---
+++ project/alumnos/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+class Alumno(models.Model):
+   
+    ...
+
+class Docente(models.Model):
+    ...
+
+class Materia(models.Model):
+    ...
+    
--- project/config/views.py
+++ project/config/views.py
@@ -1,2 +1,2 @@
 from django.http import HttpResponse
-from django.template import Context, Template
+from django.shortcuts import render
