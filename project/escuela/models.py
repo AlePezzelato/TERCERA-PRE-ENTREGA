5c67131a245688db142f3946036971aae0d32ae0
AlePezzelato <apezzelato1@gmail.com>
2024-05-13 11:26:10 -0300

    cambios en los models de escuela

--- project/escuela/models.py
+++ project/escuela/models.py
@@ -4,10 +4,17 @@ class Alumno(models.Model):
     nombre = models.CharField(max_length=150)
     apeliido = models.CharField(max_length=150)
     fecha_nacimiento = models.DateField(null=True)
+    materia_id = models.ForeignKey(Materia, on_delete=models.SET_NULL, null=True)
+    docente_id = models.ForeignKey(Docente, on_delete=models.SET_NULL, null=True)
+    nota = models.CharField(max_length=20)
+    
 
 class Docente(models.Model):
-    ...
-
+    nombre = models.CharField(max_length=150)
+    apeliido = models.CharField(max_length=150)
+    materia_id = models.ForeignKey(Materia, on_delete=models.SET_NULL, null=True)
+        
 class Materia(models.Model):
-    ...
+    nombre = models.CharField(max_length=150, unique=True)
+    codigo = models.CharField(max_length=10)
     
